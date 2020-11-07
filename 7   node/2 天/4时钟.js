4036ab595965c443c363cf18b442d4339b412863
/配置姓名 <1299898126@qq.com>
2020-11-07 21:15:08 +0800

    node第二天时钟案例

--- 7   node/2 天/4时钟.js
+++ 7   node/2 天/4时钟.js
@@ -1,6 +1,7 @@
 // 1  导入HTTP 模块
 const fs = require('fs')
 const http = require('http')
+const path = require('path')
 // 2 根据HTTP 模块创建服务器对像
 const server = http.createServer()
 // 3 监听客户端的请求
@@ -8,9 +9,9 @@ server.on('request', (req, res) => {
 	const url = req.url
 	let fpath = ''
 	if (url === '/') {
-		fpath = path.join(__dirname, 'clock', 'index.html')
+		fpath = path.join(__dirname, '/clock', 'index.html')
 	} else {
-		fpath = path.join(__dirname, 'clock', 'url')
+		fpath = path.join(__dirname, '/clock', url)
 	}
 
 	fs.readFile(fpath, (err, con) => {
